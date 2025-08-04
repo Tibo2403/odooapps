915e8d54870bee2817b0ecba3f9d2badf1ee357b
Thibault <Thibault.ahn@gmail.com>
2025-08-04 10:45:19 +0200

    fix: correct module references

--- om_hr_payroll/models/hr_payslip.py
+++ om_hr_payroll/models/hr_payslip.py
@@ -88,8 +88,8 @@ class HrPayslip(models.Model):
             copied_payslip = payslip.copy({'credit_note': True, 'name': _('Refund: ') + payslip.name})
             copied_payslip.compute_sheet()
             copied_payslip.action_payslip_done()
-        form_view_ref = self.env.ref('om_om_hr_payroll.view_hr_payslip_form', False)
-        list_view_ref = self.env.ref('om_om_hr_payroll.view_hr_payslip_tree', False)
+        form_view_ref = self.env.ref('om_hr_payroll.view_hr_payslip_form', False)
+        list_view_ref = self.env.ref('om_hr_payroll.view_hr_payslip_tree', False)
         return {
             'name': (_("Refund Payslip")),
             'view_mode': 'list, form',
